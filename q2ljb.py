1f061f5519957513d4178ea477452c28b6cf81e9
Chris Forbath <lj4bath@icloud.com>
2022-03-14 23:27:26 -0400

    thanks

---
+++ q2ljb.py
@@ -0,0 +1,103 @@
+#q2a
+import csv
+
+import sys
+
+#global itemsArray
+itemsArray = []
+
+def read_data(filename):
+    with open(filename) as csvfile:
+      next (csvfile)
+      for line in csvfile:
+        return list(csv.reader(csvfile))
+
+
+#q2b
+#print(smData)
+
+def branch_totals(itemsArray):
+    totals_dict = {}
+    A_totals = [float(line[9]) for line in itemsArray if line[1] == 'A']
+    totals_dict["A"]=sum(A_totals)
+    return totals_dict
+
+
+
+smData = read_data('supermarket_sales.csv')
+totals = branch_totals(smData)
+print(totals)
+
+#C
+def build_dict(itemsArray):
+    d={}
+
+    for item in itemsArray:
+        d[item[1][3]]
+
+
+sys.exit()
+
+
+
+#input('A', 105651.399)('B', 106197.672)('C', 110568.7065)
+#sys.exit()
+outputb = '''
+Totals by Branch:
+Branch A: 105651.399
+Branch B: 106197.672
+Branch C: 110568.7065
+'''
+print(outputb)
+
+'''
+part c
+Totals by branch and customer type
+
+
+Plan A
+br| C Type  | Total
+A | Normal | 3585
+A | Member |
+B | Normal
+B | Member
+C | Normal
+C | Member
+
+
+{"A": 123456.123, "B": 234567.567, "C": 345678.9987}
+
+#q2c
+#Q2C (list version)
+aArrayM =[]
+bArrayM =[]
+cArrayM =[]
+aArrayN =[]
+bArrayN =[]
+cArrayN =[]
+
+tList =[]
+
+for i in range(0,len(itemsArray)):
+
+  if itemsArray[i][1]=="A" and itemsArray[i][3]== "Member":
+    aArrayM.append (itemsArray[i][9])
+    tList.insert(len(tList),(A,str(sum(aArrayM))))
+  elif itemsArray[i][1]=="A" and itemsArray[i][3]== "Normal":
+    aArrayN.append (itemsArray[i][9])
+    tList.insert(len(tList),(A,str(sum(aArrayN))))
+  elif itemsArray[i][1]=="B" and itemsArray[i][3]== "Member":
+    bArrayM.append (itemsArray[i][9])
+    tList.insert(len(tList),(B,str(sum(bArrayM)))
+  elif itemsArray[i][1]=="B" and itemsArray[i][3]== "Normal":
+    bArrayN.append (itemsArray[i][9])
+    tList.insert(len(tList),(B,str(sum(bArrayN)))
+  elif itemsArray[i][1]=="C" and itemsArray[i][3]== "Member":
+    cArrayM.append (itemsArray[i][9])
+    tList.insert(len(tList),(C,str(sum(cArrayM))))
+  elif itemsArray[i][1]=="C" and itemsArray[i][3]== "Normal":
+    cArrayN.append (itemsArray[i][9])
+    tList.insert(len(tList),(C,str(sum(cArrayN))))
+
+print(tList)
+'''
