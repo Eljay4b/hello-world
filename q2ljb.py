58219090b9f6a346120aa52279291a62ac2d7ce9
P Jakobsen <pjakobsen@gmail.com>
2022-03-15 09:17:34 -0400

    outputting nice report

--- q2ljb.py
+++ q2ljb.py
@@ -40,73 +40,15 @@ smData = read_data('supermarket_sales.csv')
 totals = branch_totals(smData)
 from pprint import pprint
 pprint(report_dict(smData))
+# So now that you have a nice dict, it should be elementary to use it to print a
+# human readalbe report by accessing nested keys
+dict = report_dict(smData)
 
-
-
-
-
-sys.exit()
-
-
-
-#input('A', 105651.399)('B', 106197.672)('C', 110568.7065)
-#sys.exit()
-outputb = '''
+print(f"""
 Totals by Branch:
-Branch A: 105651.399
-Branch B: 106197.672
-Branch C: 110568.7065
-'''
-print(outputb)
-
-'''
-part c
-Totals by branch and customer type
-
-
-Plan A
-br| C Type  | Total
-A | Normal | 3585
-A | Member |
-B | Normal
-B | Member
-C | Normal
-C | Member
-
-
-{"A": 123456.123, "B": 234567.567, "C": 345678.9987}
-
-#q2c
-#Q2C (list version)
-aArrayM =[]
-bArrayM =[]
-cArrayM =[]
-aArrayN =[]
-bArrayN =[]
-cArrayN =[]
-
-tList =[]
-
-for i in range(0,len(itemsArray)):
-
-  if itemsArray[i][1]=="A" and itemsArray[i][3]== "Member":
-    aArrayM.append (itemsArray[i][9])
-    tList.insert(len(tList),(A,str(sum(aArrayM))))
-  elif itemsArray[i][1]=="A" and itemsArray[i][3]== "Normal":
-    aArrayN.append (itemsArray[i][9])
-    tList.insert(len(tList),(A,str(sum(aArrayN))))
-  elif itemsArray[i][1]=="B" and itemsArray[i][3]== "Member":
-    bArrayM.append (itemsArray[i][9])
-    tList.insert(len(tList),(B,str(sum(bArrayM)))
-  elif itemsArray[i][1]=="B" and itemsArray[i][3]== "Normal":
-    bArrayN.append (itemsArray[i][9])
-    tList.insert(len(tList),(B,str(sum(bArrayN)))
-  elif itemsArray[i][1]=="C" and itemsArray[i][3]== "Member":
-    cArrayM.append (itemsArray[i][9])
-    tList.insert(len(tList),(C,str(sum(cArrayM))))
-  elif itemsArray[i][1]=="C" and itemsArray[i][3]== "Normal":
-    cArrayN.append (itemsArray[i][9])
-    tList.insert(len(tList),(C,str(sum(cArrayN))))
-
-print(tList)
-'''
+-----------------
+Branch A: Normal ${dict['A']['Normal']}, Member ${dict['A']['Member']}
+Branch B: Normal ${dict['B']['Normal']}, Member ${dict['B']['Member']}
+Branch B: Normal ${dict['C']['Normal']}, Member ${dict['C']['Member']}
+etc.
+""")
