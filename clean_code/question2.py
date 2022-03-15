a2e9345b77f57c895f919f989328eb4da557b3b9
P Jakobsen <pjakobsen@gmail.com>
2022-03-14 21:32:08 -0400

    Added skeleton support for question 2 C & D

--- clean_code/question2.py
+++ clean_code/question2.py
@@ -29,4 +29,17 @@ C_totals = [float(line[9]) for line in data if line[1] == 'C']
 report = {"A":sum(A_totals),"B":sum(B_totals),"C":sum(C_totals)}
 print(report)
 
-# PART C
+''' PART C
+c) to receive the Supermarket Sales list and return an object (maybe a list or a dictionary) containing the total number of sales per Branch and per Customer Type.
+The returned information can be either one of the below choices or another (The numbers given below may not be correct):)
+
+a dictionary like {"A": {"Member": 230, "Normal": 351}, "B": {"Member": 123, "Normal": 117}, "C": {"Member": 335, "Normal": 18}} a list like [ [230, 351], [123, 117], [335, 18]] You need to consider the best structure to return since you will be displaying this information in a human-readable format in the following question.
+'''
+# code here
+
+'''
+PART D
+ [5 pts] Write a Python program to do the following:
+Call the function you wrote in a) to read the data file Call the function you wrote in b) to calculate the total sales information and display the information on the screen Call the function you wrote in c) to calculate the total number of sales per Branch and per Customer Type and display the information on the screen Note: While displaying the information on the screen, the outputs should be human-readable and understandable (e.g. column headings, row names)
+'''
+# code here
