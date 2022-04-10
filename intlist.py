85ee28451013c761861cb065782ec87d9fc276e8
Eljay4b <lj4bath@icloud.com>
2022-04-09 21:09:05 -0400

    Class to accept list of integers
    Mayday! I feel like a complete effing idiot. If you could please provide some guidance... Thanks.

---
+++ intlist.py
@@ -0,0 +1,11 @@
+'''
+a) [10 pts] Instantiate the Stat class with a constructor that accepts a list of integers.
+
+Each instantiated object should have two attributes: length and items. 
+
+The length returns the number of elements of the object, and the items attribute returns the content of the list.
+
+For example, if objStat = Stat([5, 3, 7, 3, 6, 4, 5, 8, 2, 9, 3, 4, 20, -10])
+then the length should be 14 and the items [5, 3, 7, 3, 6, 4, 5, 8, 2, 9, 3, 4, 20, -10]
+
+'''
