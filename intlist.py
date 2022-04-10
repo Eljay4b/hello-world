c359ea5421e16bf5e308c9f1a79fcbd950374cf3
P Jakobsen <pjakobsen@gmail.com>
2022-04-10 13:53:49 -0400

    Just did a) and b)

--- intlist.py
+++ intlist.py
@@ -7,7 +7,33 @@ The length returns the number of elements of the object, and the items attribute
 
 For example, if objStat = Stat([5, 3, 7, 3, 6, 4, 5, 8, 2, 9, 3, 4, 20, -10])
 then the length should be 14 and the items [5, 3, 7, 3, 6, 4, 5, 8, 2, 9, 3, 4, 20, -10]
+'''
 
+class Stat():
+    def __init__(self, data):
+        self.items= data
+        self.length = len(data)
+
+    # Mutator Method for sorting using insertion sort algorithm
+    def sortStat(self):
+        for i in range(1, self.length):
+            val = self.items[i]
+            pos = i
+            while pos > 0 and self.items[pos - 1] > val:
+                self.items[pos] = self.items[pos -1]
+                pos = pos - 1
+
+            self.items[pos] = val
+
+if __name__ == '__main__':
+    objStat = Stat([5, 3, 7, 3, 6, 4, 5, 8, 2, 9, 3, 4, 20, -10])
+    print(objStat.items)
+    print(objStat.length)
+    objStat.sortStat()
+    print(objStat.items)
+
+
+'''
 b) (10 Points) Define and implement a mutator method named sortStat as a member of the Stat class.
 The sortStat method sorts the items attribute elements in ascending order. Use the insertion sort algorithm for this task.
 
